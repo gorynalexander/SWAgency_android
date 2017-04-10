0e5ebe5bc3c175290e5c947ddde84e9ba70bbe7c
Odinn <goryn.alexander@gmail.com>
2017-04-10 03:33:01 +0300

    Changed design of AboutUs fragment

--- app/src/main/java/com/sw/swagency_2/fragments/AboutUsFragment.java
+++ app/src/main/java/com/sw/swagency_2/fragments/AboutUsFragment.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.sw.swagency_2.R;
 import com.sw.swagency_2.adapters.ProsAdapter;
 import com.sw.swagency_2.models.Advantage;
@@ -31,12 +33,18 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        ImageView ivLogo = (ImageView) view.findViewById(R.id.ivLogo);
+        Picasso.with(view.getContext())
+                .load(R.drawable.logotip_obrezanny)
+                .resize(250, 200)
+                .into(ivLogo);
 
-        rvPros = (RecyclerView) view.findViewById(R.id.rvPros);
-        rvLayoutManager = new GridLayoutManager(getContext(), 2);
-        rvPros.setLayoutManager(rvLayoutManager);
-        prosAdapter = new ProsAdapter(createPros());
-        rvPros.setAdapter(prosAdapter);
+//        rvPros = (RecyclerView) view.findViewById(R.id.rvPros);
+//        rvLayoutManager = new GridLayoutManager(getContext(), 2);
+//        rvPros.setLayoutManager(rvLayoutManager);
+//        prosAdapter = new ProsAdapter(createPros());
+//        rvPros.setAdapter(prosAdapter);
+//        rvPros.setNestedScrollingEnabled(false);
 
         return view;
     }
